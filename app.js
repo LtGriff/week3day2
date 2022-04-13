b61f818589a844d84599f7625c4d9f97b4ecaa1d
LtGriff <ltgriff96@gmail.com>
2022-04-13 05:20:07 -0400

    push

---
+++ app.js
@@ -0,0 +1,36 @@
+
+var a = 10;
+var b = 20;
+
+
+function addSum(param1, param2) {
+    console.log(param1 + param2)
+    return param1 + param2
+    
+}
+addSum(a,b);
+
+
+function intsubtract(param1, param2) {
+    console.log(param1 - param2)
+    return param1 - param2
+    
+}
+intsubtract(a,b);
+
+
+
+function  multiply(param1, param2) {
+    console.log(param1 * param2)
+    return param1 * param2
+    
+}
+multiply(a,b);
+
+
+function  divide(param1, param2) {
+    console.log(param1 / param2)
+    return param1 / param2
+    
+}
+divide(a,b);
